e1584cf260ebfdefefe0b420c84f8f4cbb449ee8
fjebaker <fergusbkr@gmail.com>
2024-04-16 11:10:25 +0100

    fix(code): remove KerrNewmann spacetime from E-Lz plot

--- code/circular-orbits.E-Lz.jl
+++ code/circular-orbits.E-Lz.jl
@@ -8,10 +8,6 @@ function E_Lz(m::AbstractMetric, r; q = 0.0)
     CircularOrbits.energy_angmom(m, SVector(r, π / 2))
 end
 
-function E_Lz(m::KerrNewmanMetric, r; q = 0.0)
-    CircularOrbits.energy_angmom(m, SVector(r, π / 2); q = q)
-end
-
 function plot_curve!(ax, m, rs; q = 0.0, kwargs...)
     values = E_Lz.(m, rs; q)
     Es = first.(values)
@@ -19,78 +15,44 @@ function plot_curve!(ax, m, rs; q = 0.0, kwargs...)
     lines!(ax, Lzs, Es; kwargs...)
 end
 
-m1 = KerrMetric(1.0, 0.0)
-rs1 = collect(range(3.3, 50.0, 300))
+m0 = KerrMetric(1.0, 0.0)
+rs0 = collect(range(3.3, 50.0, 300))
 
-m2 = KerrMetric(1.0, 0.4)
-rs2 = collect(range(2.5, 50.0, 300))
+m1 = KerrMetric(1.0, 0.4)
+rs1 = collect(range(2.5, 50.0, 300))
 
-m3 = KerrNewmanMetric(1.0, m2.a, 1e-8)
-rs3 = collect(range(2.5, 50.0, 300))
-# add this one by hand
-push!(rs3, 450.0)
+m2 = KerrMetric(1.0, 0.8)
+rs2 = collect(range(1.96, 50.0, 300))
 
-m4 = KerrNewmanMetric(1.0, m1.a, 1e-8)
-rs4 = collect(range(3.0, 50.0, 300))
-# add this one by hand
-push!(rs4, 450.0)
+m3 = KerrMetric(1.0, 0.998)
+rs3 = collect(Gradus.Grids._geometric_grid(1.08, 50.0, 300))
 
 begin
     palette = _default_palette()
 
-    fig = Figure(resolution = (500, 400))
+    fig = Figure(resolution = (450, 300))
     layout = fig[1, 1] = GridLayout()
     ax = Axis(layout[2, 1], xlabel = L"L_z", ylabel = L"E", yticks = LinearTicks(3))
-    ylims!(ax, 0.85, 1.23)
+    ylims!(ax, 0.65, 1.15)
     xlims!(ax, 1, 6.5)
 
-    color_1 = popfirst!(palette)
-    _ = popfirst!(palette)
-    color_2 = popfirst!(palette)
-    color_3 = popfirst!(palette)
+    s0 = plot_curve!(ax, m0, rs0, color = popfirst!(palette))
+    k1 = plot_curve!(ax, m1, rs1, linewidth = 1.5, color = popfirst!(palette))
+    k2 = plot_curve!(ax, m2, rs2, linewidth = 1.5, color = popfirst!(palette))
+    k3 = plot_curve!(ax, m3, rs3, linewidth = 1.5, color = popfirst!(palette))
 
-    s1 = plot_curve!(ax, m1, rs1, color = color_1)
-    k1 = plot_curve!(ax, m2, rs2, linestyle = :dot, linewidth = 1.5, color = color_1)
-
-    kp1 = plot_curve!(
-        ax,
-        m3,
-        rs3,
-        q = 0.9e8,
-        color = color_2,
-        linestyle = :dot,
-        linewidth = 1.5,
-    )
-    sp1 = plot_curve!(ax, m4, rs4, q = 0.9e8, color = color_2)
-
-    kn1 = plot_curve!(
-        ax,
-        m3,
-        rs3,
-        q = -0.9e8,
-        color = color_3,
-        linestyle = :dot,
-        linewidth = 1.5,
-    )
-    sn1 = plot_curve!(ax, m4, rs4, q = -0.9e8, color = color_3)
-
-    a_str = Printf.@sprintf("%0.1f", m2.a)
+    a1_str = Printf.@sprintf("%0.1f", m1.a)
+    a2_str = Printf.@sprintf("%0.1f", m2.a)
+    a3_str = Printf.@sprintf("%0.1f", m3.a)
 
     ga = layout[1, 1] = GridLayout()
     Legend(
         ga[1, 1],
-        [s1, k1],
-        [L"a = 0.0", L"a = %$(a_str)"],
+        [k3, k2, k1, s0],
+        [L"a = %$(a)" for a in reverse(("0.0", a1_str, a2_str, a3_str))],
         orientation = :horizontal,
         framevisible = false,
         height = 10,
-    )
-    Legend(
-        ga[2, 1],
-        [sp1, s1, sn1],
-        [L"qQ = 1", L"qQ = 0", L"qQ = -1"],
-        orientation = :horizontal,
-        framevisible = false,
     )
     colgap!(ga, 0)
     rowgap!(ga, 0)
