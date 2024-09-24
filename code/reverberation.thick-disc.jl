478f96cdac0306a697450d79ce3cb730ab12230b
Fergus Baker <fergusbkr@gmail.com>
2024-09-24 21:57:06 +0100

    feat: thick disc transfer function sheet

--- code/reverberation.thick-disc.jl
+++ code/reverberation.thick-disc.jl
@@ -13,7 +13,7 @@ function lag_energy(data, flow, fhi)
 end
 
 function lag_frequency_rowwise(t, f::AbstractMatrix; flo = 1e-5, kwargs...)
-    map(eachrow(f)) do ψ
+    Gradus._threaded_map(eachrow(f)) do ψ
         t_extended, ψ_extended = Gradus.extend_domain_with_zeros(t, ψ, 1 / flo)
         Gradus.lag_frequency(t_extended, ψ_extended; kwargs...)
     end
@@ -36,7 +36,7 @@ function calculate_2d_transfer_function(m, x, model, itb, prof, radii)
         rmin = minimum(radii),
         rmax = maximum(radii),
         h = 1e-8,
-        g_grid_upscale = 3,
+        g_grid_upscale = 1,
     )
 
     flux[flux.==0] .= NaN
@@ -54,46 +54,95 @@ function calculate_lag_transfer(m, x, d, model, radii, itb)
     (; freq, τ, ψ, t, all_pulses, E, f)
 end
 
+function calculate_for_disc(m, d)
+    model = LampPostModel(h = 10.0)
+    # different angles
+    angles = [10, 30, 60, 80]
+    datas = map(angles) do angle
+        @info "Angle = $angle"
+        x = SVector(0.0, 10_000.0, deg2rad(angle), 0.0)
+        b0 = angle < 45 ? 1.0 : 2.0
+        itb = Gradus.interpolated_transfer_branches(m, x, d, radii; verbose = true, β₀ = b0, abstol = 1e-12, reltol = 1e-12)
+        calculate_lag_transfer(m, x, d, model, radii, itb)
+    end
+    datas
+end
+
 m = KerrMetric(1.0, 0.998)
-angles = collect(5:10:85)
-radii = Gradus.Grids._geometric_grid(Gradus.isco(m), 1000.0, 300)
+radii = Gradus.Grids._geometric_grid(Gradus.isco(m), 1000.0, 351)
 
-model = LampPostModel(h = 10.0)
-d = ShakuraSunyaev(m)
+begin
+    @info "Thin disc"
+    datas1 = calculate_for_disc(m, ThinDisc(0.0, Inf))
+    @info "Thick disc 1"
+    datas2 = calculate_for_disc(m, ShakuraSunyaev(m, eddington_ratio = 0.1))
+    @info "Thick disc 2"
+    datas3 = calculate_for_disc(m, ShakuraSunyaev(m, eddington_ratio = 0.2))
+    @info "Thick disc 3"
+    datas4 = calculate_for_disc(m, ShakuraSunyaev(m, eddington_ratio = 0.3))
+end
 
-# different angles
-# itbs = map(angles) do angle
-#     @info "Angle = $angle"
-#     x = SVector(0.0, 10_000.0, deg2rad(angle), 0.0)
-#     b0 = angle < 45 ? 1.0 : 2.0
-#     itb = Gradus.interpolated_transfer_branches(m, x, d, radii; verbose = true, β₀ = b0)
-# end ;
+begin
+    palette = _default_palette()
 
-# datas = map(zip(angles, itbs)) do it
-#     angle, itb = it
-#     @info "Angle = $angle"
-#     x = SVector(0.0, 10_000.0, deg2rad(angle), 0.0)
-#     freq1, τ1, impulse1, time1 = calculate_lag_transfer(m, x, d, model, radii, itb)
-# end ;
+    fig = Figure(size = (800, 600))
+    ga = fig[1, 1] = GridLayout()
 
-# different rates
-rates = [0.05, 0.1, 0.2, 0.3]
-itbs = map(rates) do rate
-    x = SVector(0.0, 10_000.0, deg2rad(60), 0.0)
-    d = ShakuraSunyaev(m; eddington_ratio = rate)
-    itb = Gradus.interpolated_transfer_branches(m, x, d, radii; verbose = true, β₀ = 2.0, abstol = 1e-12, reltol = 1e-12)
-end ;
+    # frequency ranges for the energy calculations
+    lims1 = (1e-3, 2e-3)
+    lims2 = (4e-3, 8e-3)
+    lims3 = (1.9e-2, 4e-2)
+    lims4 = (1e-5, 1e-4)
 
-datas = map(zip(rates, itbs)) do it
-    rate, itb = it
-    x = SVector(0.0, 10_000.0, deg2rad(60), 0.0)
-    d = ShakuraSunyaev(m; eddington_ratio = rate)
-    calculate_lag_transfer(m, x, d, model, radii, itb)
-end ;
+    # x_lims1 = (0.83, 1.08)
+    x_lims2 = (0.45, 1.45)
+    x_lims1 = (0.45, 1.48)
+
+    axes = map(enumerate((datas2, datas3, datas4))) do (col, ds)
+        map(enumerate(ds[3:end])) do (row, d)
+            ax = Axis(ga[row, col])
+            thin = datas1[row + 2]
+
+            for (lim) in ((lims1, lims2, lims3, lims4))
+                thin_le = lag_energy(thin.all_pulses, lim...)
+                lines!(ax, thin.E, thin_le, color = :grey, alpha=0.3)
+
+                le = lag_energy(d.all_pulses, lim...)
+                lines!(ax, d.E, le)
+            end
+            if col != 1
+                hideydecorations!(ax, grid=false)
+            end
+            if row != 2 && row != 4
+                hidexdecorations!(ax, grid=false)
+            else
+                if row == 2
+                    xlims!(ax, x_lims1...)
+                else
+                    xlims!(ax, x_lims2...)
+                end
+            end
+            ax
+        end
+    end
+    axs = reshape(reduce(vcat, axes), (length(axes[1]), length(axes)))
+    for i in 1:length(axes)
+        linkxaxes!(axs[1, i], axs[2, i])
+        # linkxaxes!(axs[3, i], axs[4, i])
+    end
+    for i in 1:length(axes[1])
+        root = axs[i, 1]
+        for a in axs[i, 2:end]
+            linkyaxes!(root, a)
+        end
+    end
+
+    fig
+end
 
 begin
     for (n,dd) in zip(rates, datas)
-        p = Plots.heatmap(dd.t, dd.E, log10.(dd.f), xlims = (0, 2000), title="$n")
+        p = Plots.heatmap(dd.t, dd.E, log10.(dd.f), xlims = (0, 200), title="$n")
         display(p)
     end
 end
@@ -117,11 +166,13 @@ begin
     fig = Figure()
     ax = Axis(fig[1,1])
 
-    for le in lagens[[1]]
-        # lines!(ax, datas[1].E, le[1])
-        # lines!(ax, datas[1].E, le[2])
-        # lines!(ax, datas[1].E, le[3])
-        lines!(ax, datas[1].E, le[4])
+    palette = _default_palette()
+    for (i, le) in enumerate(lagens)
+        c = popfirst!(palette)
+        lines!(ax, datas[1].E, le[1], color = c)
+        lines!(ax, datas[1].E, le[2], color = c)
+        lines!(ax, datas[1].E, le[3], color = c)
+        # lines!(ax, datas[1].E, le[4], color = c)
     end
     fig
 end
